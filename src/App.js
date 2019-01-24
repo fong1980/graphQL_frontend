efb1da0f2e89fdb4a39c117dbbc3593c481b4c33
Fong <fong@videodock.com>
2019-01-24 17:14:05 +0100

    chore(app): add Apollo package, create ApolloWrapper

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import Booklist from './components/Booklist';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4001/graphql',
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      react app
-        <Booklist />
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          react app
+          <Booklist />
+        </div>
+      </ApolloProvider>
     );
   }
 }
 
-export default App;
+export default (App);
--- src/components/Booklist.js
+++ src/components/Booklist.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
 
 class Booklist extends Component {
   render() {
