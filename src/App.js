2c6c534756709178326e2bb874a196bc8173d82a
Fong <fong@videodock.com>
2019-01-24 16:44:50 +0100

    chore(app): basic setup, cleanup, create Booklist component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import Booklist from './components/Booklist';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">react app
+      <div className="App">
+      react app
+        <Booklist />
       </div>
     );
   }
---
+++ src/components/Booklist.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Booklist extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ul id="book-list">
+          <li>Book name</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Booklist;
