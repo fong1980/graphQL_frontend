cd139b611e8de722a8cd3b077996de752f7fc9a8
Fong <fong@videodock.com>
2019-01-25 11:21:30 +0100

    chore(AddBook): submit to the state succeed

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
-import { getAuthorsQuery } from './../queries/queries';
+import { getAuthorsQuery, getBooksQuery } from './../queries/queries';
 
 class AddBook extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+
+      name    : '',
+      genre   : '',
+      authorId: '',
+    };
+  }
   displayAuthors() {
     const data = this.props.data;
 
@@ -15,6 +24,12 @@ class AddBook extends Component {
     });
   }  
 
+  submitForm(event) {
+    event.preventDefault();
+
+    console.log(this.state); 
+  }
+
   render() {
     if(this.props.data.loading){
       return <div>loading</div>;
@@ -22,18 +37,18 @@ class AddBook extends Component {
     // console.log(this.props.data.books);
   
     return (
-      <form id="add-book">
+      <form id="add-book" onSubmit={ this.submitForm.bind(this) }>
         <div className="field">
           <label>Book name:</label>
-          <input type="text" />
+          <input type="text" onChange={ e => this.setState({ name: e.target.value })} />
         </div>
         <div className="field">
           <label>Genre:</label>
-          <input type="text" />
+          <input type="text" onChange={ e => this.setState({ genre: e.target.value })} />
         </div>
         <div className="field">
           <label>Author:</label>
-          <select>
+          <select onChange={ e => this.setState({ authorId: e.target.value })} >
             <option>Select author</option>
             { this.displayAuthors() }
           </select>
