0718596ca5eac23337401049efe8339a0368ee84
Fong <fong@videodock.com>
2019-01-25 10:50:43 +0100

    chore(AddBook): getAuthor query done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import ApolloProvider from 'react-apollo/ApolloProvider';
 import Booklist from './components/Booklist';
+import AddBook from './components/AddBook';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
@@ -14,6 +15,7 @@ class App extends Component {
         <div className="App">
           react app
           <Booklist />
+          <AddBook />
         </div>
       </ApolloProvider>
     );
