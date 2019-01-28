9fcef64f2d71bed154bb94e5b6c57ab2d4d70d24
Fong <fong@videodock.com>
2019-01-28 11:07:33 +0100

    chore(BookDetails): add BookDetails

---
+++ src/components/BookDetails.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import { graphql, compose } from 'react-apollo';
+// import { getAuthorsQuery, addBookMutation, getBooksQuery } from '../queries/queries';
+
+// https://www.youtube.com/watch?v=PLZNt5Ov36I&list=PL4cUxeGkcC9iK6Qhn-QLcXCXPQUov1U7f&index=34 <-- start from here
